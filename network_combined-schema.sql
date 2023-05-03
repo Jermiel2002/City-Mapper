c02b58257d558758a31072f3c0b820853d32ee07
jermiel <kpessou.kounouho@edu.univ-paris13.fr>
2023-05-03 21:31:28 +0200

    combinaisons et noeuds

---
+++ network_combined-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE network_combined (
+	from_stop_I		integer,
+	to_stop_I		integer,
+	d			integer,
+	duration_avg		text,
+	n_vehicles		integer,
+	route_I_counts		integer,
+	route_type		integer
+	);
---
+++ network_nodes-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE network_nodes (
+	stop_I		integer,
+	latitude	numeric (21,17),
+	longitude	numeric (21,17),
+	stop_name	text
+	);
